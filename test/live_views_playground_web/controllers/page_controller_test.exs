6cd2454533e56e87e3b055c7cb490c196e86a1a6
Andrew Sndier <andrewsnider15@gmail.com>
2023-07-30 18:41:56 -0700

    fix page controller smoke test I broke a while ago

--- test/live_views_playground_web/controllers/page_controller_test.exs
+++ test/live_views_playground_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LiveViewsPlaygroundWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Live Views Playground"
   end
 end
